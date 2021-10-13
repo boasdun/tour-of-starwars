36ee1697e667d4c56a0131595c266082de103837
Boas Dun <boas.dun@nubisonline.nl>
2021-10-13 14:33:14 +0200

    03 Create a Feature Component

---
+++ src/app/character-detail/character-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Character } from '../character';
+
+@Component({
+  selector: 'app-character-detail',
+  templateUrl: './character-detail.component.html',
+  styleUrls: ['./character-detail.component.scss']
+})
+export class CharacterDetailComponent implements OnInit {
+  @Input() character?: Character;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
